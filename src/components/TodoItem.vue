d3db2c3ae6d486e6f1a32ecc86dc14ac94b9b14b
iancheng <iancheng0323@gmail.com>
2019-08-01 20:03:24 +0800

    yo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li><input type="checkbox" v-bind:checked="checked()">{{msg}}</li>
+</template>
+
+<script>
+export default {
+    name:'TodoItem',
+    props:[
+        'msg',
+        'status'
+    ],
+    computed:{
+        checked: function(){
+            if( this.status == 1 ){
+                return true
+            }else{
+                return false
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -2,22 +2,62 @@
   <div class="todo-main">
     <h1>Todos</h1>
     <ul>
+      <li is="TodoItem" v-for="todo in sampleTodoList" :msg="todo.msg" :key="todo.key" :status="todo.status"></li>
+    </ul>
+    <input v-model="inputMsg" type="text" placeholder="something">
+    <button @click="addTodo">Add Todo</button>
+    <h1>Completed</h1>
+    <ul>
       <li>foo</li>
       <li>bar</li>
       <li>pho</li>
       <li>now</li>
-
     </ul>
-    <h1>Completed</h1>
   </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue'
+
 export default {
   name: 'TodoMain',
+  components:{
+    TodoItem
+  },
   data: function(){
     return{
+      sampleTodoList:[
+        {
+          msg: 'foo',
+          status: 0,
+          key:1
+        },
+        {
+          msg: 'bar',
+          status: 1,
+          key:2
+        },
+        {
+          msg: 'pho',
+          status: 1,
+          key:3
+        }
+      ],
+      inputMsg:'',
 
+    }
+  },
+  methods:{
+    addTodo: function(){
+      // eslint-disable-next-line
+      console.log(this.inputMsg)
+      this.sampleTodoList.push({
+        msg: this.inputMsg,
+        status: 0,
+        key: this.sampleTodoList.length+1
+      })
+      this.inputMsg = ''
+      
     }
   }
 }
